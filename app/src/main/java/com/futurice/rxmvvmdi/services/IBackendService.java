0ad28f5da193428c6b51b0780d4faff4f6dbe0b7
Olli Aleksi Tapio Salonen <olli.salonen@futurice.com>
2016-05-09 12:56:24 +0200

    User endpoint.

--- app/src/main/java/com/futurice/rxmvvmdi/services/IBackendService.java
+++ app/src/main/java/com/futurice/rxmvvmdi/services/IBackendService.java
@@ -1,13 +1,18 @@
 package com.futurice.rxmvvmdi.services;
 
 import com.futurice.rxmvvmdi.models.Post;
+import com.futurice.rxmvvmdi.models.User;
 
 import java.util.List;
 
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import rx.Observable;
 
 public interface IBackendService {
     @GET("posts")
     Observable<List<Post>> getPosts();
+
+    @GET("users/{userId}")
+    Observable<User> getUser(@Path("userId") int userId);
 }
