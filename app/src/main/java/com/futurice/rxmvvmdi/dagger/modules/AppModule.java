4d330b63a6edc9a9523e036fe767b09fa458c661
Olli Aleksi Tapio Salonen <olli.salonen@futurice.com>
2016-05-10 11:07:20 +0200

    Remove unused imports.

--- app/src/main/java/com/futurice/rxmvvmdi/dagger/modules/AppModule.java
+++ app/src/main/java/com/futurice/rxmvvmdi/dagger/modules/AppModule.java
@@ -1,19 +1,12 @@
 package com.futurice.rxmvvmdi.dagger.modules;
 
-import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.futurice.rxmvvmdi.RxMvvmApp;
-import com.futurice.rxmvvmdi.services.IBackendService;
 import com.futurice.rxmvvmdi.services.SystemMonitorService;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-import rx.schedulers.Schedulers;
 
 @Module
 public class AppModule {
